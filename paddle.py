28747463aec21ba26fe32573e49a7e700958ab2c
Chitiwat Phajan <chitiwat2547@gmail.com>
2024-12-15 12:57:04 +0700

    now paddle(player) have margin to highlight player

--- paddle.py
+++ paddle.py
@@ -10,23 +10,49 @@ class Paddle:
         self.my_turtle.setheading(0)
         self.my_turtle.hideturtle()
 
+
     def set_location(self, location):
         self.location = location
         self.my_turtle.goto(self.location[0], self.location[1])
 
     def draw(self):
-        self.my_turtle.color(self.color)
-        self.my_turtle.goto(self.location[0], self.location[1] - self.height / 2)
-        self.my_turtle.forward(self.width / 2)
+        margin = 2.5  # ความกว้างของขอบ
+
+        # วาดขอบ (สีขอบ)
+        border_color = (0,0,0)  # สีของขอบ
+        self.my_turtle.color(border_color)
+        self.my_turtle.penup()
+        self.my_turtle.goto(self.location[0] - self.width / 2,
+                            self.location[1] - self.height / 2)  # มุมซ้ายล่าง
         self.my_turtle.pendown()
         self.my_turtle.begin_fill()
         for _ in range(2):
+            self.my_turtle.forward(self.width)
             self.my_turtle.left(90)
             self.my_turtle.forward(self.height)
             self.my_turtle.left(90)
-            self.my_turtle.forward(self.width)
         self.my_turtle.end_fill()
         self.my_turtle.penup()
+
+        # วาด paddle จริง (พื้นที่ภายใน)
+        self.my_turtle.color(self.color)
+        inner_width = self.width - 2 * margin
+        inner_height = self.height - 2 * margin
+        self.my_turtle.goto(
+            self.location[0] - inner_width / 2,
+            self.location[1] - inner_height / 2
+        )  # มุมซ้ายล่างของ paddle จริง
+        self.my_turtle.pendown()
+        self.my_turtle.begin_fill()
+        for _ in range(2):
+            self.my_turtle.forward(inner_width)
+            self.my_turtle.left(90)
+            self.my_turtle.forward(inner_height)
+            self.my_turtle.left(90)
+        self.my_turtle.end_fill()
+        self.my_turtle.penup()
+
+        # กลับไปที่ตำแหน่งศูนย์กลางของ paddle
         self.my_turtle.goto(self.location[0], self.location[1])
 
     def clear(self):
